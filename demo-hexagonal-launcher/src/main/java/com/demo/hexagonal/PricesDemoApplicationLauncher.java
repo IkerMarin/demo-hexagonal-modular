d7d8c71e58b8c5db79920389339ce7e0bed42abf
Iker Marin <IkerMarin@users.noreply.github.com>
2024-03-03 21:04:18 +0100

    feat: new launcher module for execution

---
+++ demo-hexagonal-launcher/src/main/java/com/demo/hexagonal/PricesDemoApplicationLauncher.java
@@ -0,0 +1,13 @@
+package com.demo.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class PricesDemoApplicationLauncher {
+	public static void main(String[] args) {
+        SpringApplication.run(PricesDemoApplicationLauncher.class);
+	}
+}
